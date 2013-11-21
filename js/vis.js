fd10b6bc7219fbb6957a8c010d9a7ca6abca25d3
Jade Dominguez <plusjade@gmail.com>
2013-11-20 21:15:42 -0800

    Heavy refactor.
    Code is more DRY and infers configuration from data payload.

--- js/vis.js
+++ js/vis.js
@@ -1,36 +1,13 @@
-var BubbleChart, root,
-  __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
-
-BubbleChart = (function() {
+var BubbleChart = (function() {
 
   function BubbleChart(data) {
-    this.hide_details = __bind(this.hide_details, this);
-
-    this.show_details = __bind(this.show_details, this);
-
-    this.hide_years = __bind(this.hide_years, this);
-
-    this.display_years = __bind(this.display_years, this);
-
-    this.move_towards_year = __bind(this.move_towards_year, this);
-
-    this.display_by_year = __bind(this.display_by_year, this);
-
-    this.move_towards_center = __bind(this.move_towards_center, this);
-
-    this.display_group_all = __bind(this.display_group_all, this);
-
-    this.start = __bind(this.start, this);
-
-    this.create_vis = __bind(this.create_vis, this);
-
-    this.create_nodes = __bind(this.create_nodes, this);
-
+    var sectionWidth = 100;
     var max_amount;
     this.data = data;
-    this.width = 940;
+    this.width = 1000;
     this.height = 600;
-    this.tooltip = CustomTooltip("gates_tooltip", 240);
+    this.padding = 100;
+    this.segmentWidth = 200;
     this.center = {
       x: this.width / 2,
       y: this.height / 2
@@ -41,198 +18,137 @@ BubbleChart = (function() {
     this.nodes = [];
     this.force = null;
     this.circles = null;
-    this.fill_color = d3.scale.ordinal().domain(["low", "medium", "high"]).range(["#6B238E", "#8A2BE2", "#BF5FFF"]);
-    max_amount = d3.max(this.data, function(d) {
-      return parseInt(d.total_amount);
-    });
+    var fill_color = d3.scale.ordinal().domain(["low", "medium", "high"]).range(["#6B238E", "#8A2BE2", "#BF5FFF"]);
+    
+    max_amount = d3.max(this.data, function(d) { return parseInt(d.total_amount) });
     this.radius_scale = d3.scale.pow().exponent(0.5).domain([0, max_amount]).range([2, 85]);
-    this.create_nodes();
-    this.create_vis();
-  }
 
-  BubbleChart.prototype.create_nodes = function() {
     var _this = this;
+
+    // nodes
     this.data.forEach(function(d) {
       var node;
       node = {
         id: d.id,
-        radius: 20,
+        radius: 16,
         department: d.department,
         x: Math.random() * 900,
         y: Math.random() * 800
       };
       return _this.nodes.push(node);
     });
-    return this.nodes.sort(function(a, b) {
-      return b.value - a.value;
-    });
+    this.nodes.sort(function(a, b) { return b.value - a.value; });
+
+    this.force = d3.layout.force().nodes(this.nodes).size([this.width, this.height]);
+
+    var that;
+    that = this;
+
+    this.vis = d3.select("#vis").append("svg")
+      .attr("height", this.height)
+      .attr("id", "svg_vis");
+
+    // DATA
+    this.circles = this.vis.selectAll("circle").data(this.nodes, function(d) { return d.id });
+
+    // ENTER
+    this.circles.enter()
+      .append("circle")
+      .attr("r", 0)
+      .attr("fill", function(d) {
+        return fill_color(d.group);
+      })
+      .attr("stroke-width", 2).attr("stroke", function(d) {
+        return d3.rgb(fill_color(d.group)).darker();
+      })
+      .attr("id", function(d) {
+        return "bubble_" + d.id;
+      })
+
+    // UPDATE
+    this.circles
+      .transition().duration(2000)
+      .attr("r", function(d) { return d.radius });
+  }
+
+
+  BubbleChart.prototype.assemble = function() {
+    var _this = this;
+
+    this.vis.selectAll(".years").remove();
+
+    _this.vis.transition().duration(500)
+      .attr("width", this.width)
+
+    this.force
+      .gravity(this.layout_gravity)
+      .charge(this.charge).friction(0.9)
+      .on("tick", function(e) {
+        _this.circles.each(function(d) {
+             d.x = d.x + (_this.center.x - d.x) * (_this.damper + 0.02) * e.alpha;
+             d.y = d.y + (_this.center.y - d.y) * (_this.damper + 0.02) * e.alpha;
+          })
+          .attr("cx", function(d) { return d.x })
+          .attr("cy", function(d) { return d.y });
+      })
+      .start()
   };
 
-  BubbleChart.prototype.create_vis = function() {
-    var that,
-      _this = this;
-    this.vis = d3.select("#vis").append("svg").attr("width", this.width).attr("height", this.height).attr("id", "svg_vis");
-    this.circles = this.vis.selectAll("circle").data(this.nodes, function(d) {
-      return d.id;
-    });
-    that = this;
-    this.circles.enter().append("circle").attr("r", 0).attr("fill", function(d) {
-      return _this.fill_color(d.group);
-    }).attr("stroke-width", 2).attr("stroke", function(d) {
-      return d3.rgb(_this.fill_color(d.group)).darker();
-    }).attr("id", function(d) {
-      return "bubble_" + d.id;
-    }).on("mouseover", function(d, i) {
-      return that.show_details(d, i, this);
-    }).on("mouseout", function(d, i) {
-      return that.hide_details(d, i, this);
-    });
-    return this.circles.transition().duration(2000).attr("r", function(d) {
-      return d.radius;
-    });
+
+  // Disperse into clusters grouped by <key>
+  BubbleChart.prototype.disperse = function(key) {
+    var _this = this;
+    var keys = _.uniq(_.pluck(_this.data, key));
+    var newWidth = keys.length*this.segmentWidth;
+    var mid = this.segmentWidth/2;
+
+    // evenly distribute the cluster points
+    var cluster_points = {};
+    keys.forEach(function(value, i) {
+      cluster_points[value] = {
+        x: (_this.segmentWidth*i + mid),
+        y: _this.height/2
+      };
+    })
+
+    console.log(_.pluck(cluster_points, 'x'))
+
+    // UPDATE viewport
+    _this.vis.transition().duration(500)
+      .attr("width", newWidth)
+
+    // UPDATE labels
+    var nodes = _this.vis.selectAll(".years").data(keys);
+    nodes.enter().append("text")
+      .attr("class", "years")
+      .attr("x", function(d) { return cluster_points[d].x })
+      .attr("y", 40)
+      .attr("text-anchor", "start")
+      .text(function(d) { return d });
+
+    // UPDATE nodes
+    this.force
+      .size([newWidth, this.height])
+      .gravity(this.layout_gravity)
+      .charge(this.charge)
+      .friction(0.9)
+      .on("tick", function(e) {
+        _this.circles.each(function(d) {
+            var point = cluster_points[d[key]];
+            d.x = d.x + (point.x - d.x) * (_this.damper + 0.02) * e.alpha * 1.1;
+            d.y = d.y + (point.y - d.y) * (_this.damper + 0.02) * e.alpha * 1.1;
+          })
+          .attr("cx", function(d) { return d.x })
+          .attr("cy", function(d) { return d.y });
+      })
+      .start()
   };
 
   BubbleChart.prototype.charge = function(d) {
     return -Math.pow(d.radius, 2.0) / 8;
   };
 
-  BubbleChart.prototype.start = function() {
-    return this.force = d3.layout.force().nodes(this.nodes).size([this.width, this.height]);
-  };
-
-  BubbleChart.prototype.display_group_all = function() {
-    var _this = this;
-    this.force.gravity(this.layout_gravity).charge(this.charge).friction(0.9).on("tick", function(e) {
-      return _this.circles.each(_this.move_towards_center(e.alpha)).attr("cx", function(d) {
-        return d.x;
-      }).attr("cy", function(d) {
-        return d.y;
-      });
-    });
-    this.force.start();
-    return this.hide_years();
-  };
-
-  BubbleChart.prototype.move_towards_center = function(alpha) {
-    var _this = this;
-    return function(d) {
-      d.x = d.x + (_this.center.x - d.x) * (_this.damper + 0.02) * alpha;
-      return d.y = d.y + (_this.center.y - d.y) * (_this.damper + 0.02) * alpha;
-    };
-  };
-
-  BubbleChart.prototype.determine_n_cluster_points_for_key_values = function(n, key_values) {
-    var cluster_points = {};
-    var i;
-    for (i = 1; i<=n; i++) {
-      var key_value = key_values[i-1];
-      cluster_points[key_value] = {
-        x: this.width*(i/(n+1)),
-        y: this.height/2
-      };
-    }
-    return cluster_points;
-  };
-
-  BubbleChart.prototype.sort_into_n_sections_for_key_with_values = function(n, key, key_values) {
-    var _this = this;
-    var cluster_points = _this.determine_n_cluster_points_for_key_values(n, key_values);
-    this.force
-      .gravity(this.layout_gravity)
-      .charge(this.charge)
-      .friction(0.9)
-      .on("tick", function(e) {
-        return _this.circles.each(_this.move_towards_cluster_points(e.alpha, cluster_points, key, key_values))
-          .attr("cx", function(d) {
-            return d.x;
-          })
-          .attr("cy", function(d) {
-            return d.y;
-          });
-      });
-    this.force.start();
-  };
-
-  BubbleChart.prototype.move_towards_cluster_points = function(alpha, cluster_points, key, key_values) {
-    var _this = this;
-    return function(d) {
-      var cluster_point = cluster_points[d[key]];
-      d.x = d.x + (cluster_point.x - d.x) * (_this.damper + 0.02) * alpha * 1.1;
-      return d.y = d.y + (cluster_point.y - d.y) * (_this.damper + 0.02) * alpha * 1.1;
-    };
-  };
-
-  BubbleChart.prototype.display_years = function() {
-    var years, years_data, years_x,
-      _this = this;
-    years_x = {
-      "2007": 80,
-      "2008": 160,
-      "2009": this.width / 2,
-      "2010": this.width - 160
-    };
-    years_data = d3.keys(years_x);
-    years = this.vis.selectAll(".years").data(years_data);
-    return years.enter().append("text").attr("class", "years").attr("x", function(d) {
-      return years_x[d];
-    }).attr("y", 40).attr("text-anchor", "middle").text(function(d) {
-      return d;
-    });
-  };
-
-
-  BubbleChart.prototype.hide_years = function() {
-    var years;
-    return years = this.vis.selectAll(".years").remove();
-  };
-
-  // BubbleChart.prototype.show_details = function(data, i, element) {
-  //   var content;
-  //   d3.select(element).attr("stroke", "black");
-  //   content = "<span class=\"name\">Title:</span><span class=\"value\"> " + data.name + "</span><br/>";
-  //   content += "<span class=\"name\">Amount:</span><span class=\"value\"> $" + (addCommas(data.value)) + "</span><br/>";
-  //   content += "<span class=\"name\">Year:</span><span class=\"value\"> " + data.year + "</span>";
-  //   return this.tooltip.showTooltip(content, d3.event);
-  // };
-
-  // BubbleChart.prototype.hide_details = function(data, i, element) {
-  //   var _this = this;
-  //   d3.select(element).attr("stroke", function(d) {
-  //     return d3.rgb(_this.fill_color(d.group)).darker();
-  //   });
-  //   return this.tooltip.hideTooltip();
-  // };
 
   return BubbleChart;
 
 })();
-
-root = typeof exports !== "undefined" && exports !== null ? exports : this;
-
-$(function() {
-  var chart, render_vis, _this = this;
-  chart = null;
-  render_vis = function(csv) {
-    chart = new BubbleChart(csv);
-    chart.start();
-    return root.display_all();
-  };
-  root.display_all = function() {
-    return chart.display_group_all();
-  };
-  root.display_year = function() {
-    // var years = ["2007", "2008", "2009", "2010"];
-    // return chart.sort_into_n_sections_for_key_with_values(4, "year", years);
-    var departments = ["Engineering", "Sales"];
-    return chart.sort_into_n_sections_for_key_with_values(2, "department", departments);
-  };
-  root.toggle_view = function(view_type) {
-    if (view_type === 'year') {
-      return root.display_year();
-    } else {
-      return root.display_all();
-    }
-  };
-  return d3.csv("data/dummy.csv", render_vis);
-});
-
